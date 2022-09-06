6407761eab07f8aa09f115aa837f7e66ec4baaf8
jkjamies <jkjamies@mtu.edu>
2022-09-06 18:41:02 -0400

    adding a note that the api key is dead

--- core/network/src/main/java/com/nameless/network/ApiRoutes.kt
+++ core/network/src/main/java/com/nameless/network/ApiRoutes.kt
@@ -1,7 +1,7 @@
 package com.nameless.network
 
 object ApiRoutes {
-    // get your own! =)
+    // get your own! =) note: this one is dead, so don't bother trying to use it
     const val API_KEY = "7737e6c2d472f9a042573213e9452413"
     // defaultRequest could also work in ApiService within the client
     const val BASE_URL = "https://api.openweathermap.org/data/2.5"
